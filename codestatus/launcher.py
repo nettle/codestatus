c0fca30eb81bd7b2a1c234d205ebd66965cc05b9
Alex <alexkrapivin@gmx.com>
2020-05-11 15:13:56 +0200

    Add codestatus launcher

---
+++ codestatus/launcher.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+
+"""
+CodeStatus script
+
+This script collects code metrics
+FIXME: description
+"""
+
+from __future__ import print_function
+import argparse
+# import datetime
+# import glob
+import logging
+import os
+# import re
+import shlex
+# import shutil
+import subprocess
+
+import folder
+
+
+CODESTATUS_EXAMPLES = """
+examples:
+
+  # "Dry run" to see commands without execution:
+  codestatus ?
+"""
+# FIXME: examples
+
+
+class CodeStatus(object):
+    """
+    This class is a launcher
+    FIXME: class description
+    """
+    def parse_args(self):
+        """
+        Parse command line arguments or show help.
+        """
+        parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
+                                         description=__doc__,
+                                         epilog=CODESTATUS_EXAMPLES)
+        parser.add_argument("-v", "--verbosity",
+                            default=0,
+                            action="count",
+                            help="Increase output verbosity (e.g., -v or -vv)")
+        parser.add_argument("-n", "--dry-run",
+                            default=False,
+                            action="store_true",
+                            help="Just print CodeStatus commands without running them")
+        parser.add_argument("--log-format",
+                            default="[codestatus] %(levelname)5s: %(message)s",
+                            help=argparse.SUPPRESS)
+        parser.add_argument("path",
+                            nargs="?",
+                            default="./",
+                            help="Path to the source code folder")
+
+        self.options = parser.parse_args()
+
+        if self.options.verbosity >= 2:
+            log_level = logging.DEBUG
+        elif self.options.verbosity >= 1:
+            log_level = logging.INFO
+        else:
+            log_level = logging.WARN
+        logging.basicConfig(level=log_level, format=self.options.log_format)
+
+        logging.debug("Options: %s", self.options)
+
+        if not os.path.isdir(self.options.path):
+            logging.error("The path specified does not exist")
+            exit(1)
+
+        logging.debug("Folder: %s", folder.fofofo())
+
+
+    def run_command(self, command, stdout=subprocess.PIPE, stderr=subprocess.PIPE):
+        """
+        Run shell command
+        """
+        if type(command) is list:
+            cmd = command
+        else:
+            cmd = shlex.split(command)
+        try:
+            process = subprocess.Popen(cmd, stdout=stdout, stderr=stderr)
+            out, err = process.communicate()
+            return out
+        except KeyboardInterrupt:
+            logging.error("Interrupted!")
+
+
+    def run(self):
+        """
+        Run CodeStatus
+        """
+        self.parse_args()
+        logging.debug("Running: sw_metrics...")
+        self.run_command("sw_metrics " + self.options.path)
+        template = """--template='{\n\t"file": "{file}",\n\t"line": "{line}",\n\t"column": "{column}",\n\t"callstack": "{callstack}",\n\t"text": "{inconclusive:text}",\n\t"severity": "{severity}",\n\t"message": "{message}",\n\t"id": "{id}",\n\t"cwe": "{cwe}",\n\t"code": "{code}"\n},'"""
+        logging.debug("Running: cppcheck...")
+        self.run_command("cppcheck -q " + template + " " + self.options.path)
+        logging.debug("Done")
+
+
+if __name__ == "__main__":
+    codestatus = CodeStatus()
+    codestatus.run()
