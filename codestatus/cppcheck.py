cb63832ff1e1a5c69b4f3ee8a28db3b8be236096
Alex <alexkrapivin@gmx.com>
2020-05-12 16:47:06 +0200

    Add placeholder for cppcheck

---
+++ codestatus/cppcheck.py
@@ -0,0 +1,13 @@
+"""
+Cppcheck is a static analysis tool for C/C++ code
+
+Requires: cppcheck version 1.70 (1.90? 2.0?)
+
+Install in Ubuntu:
+
+    sudo apt-get install cppcheck
+
+Download:
+- http://cppcheck.sourceforge.net/
+- https://github.com/danmar/cppcheck
+"""
